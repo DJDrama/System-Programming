4176a6c46a6fab83e947127aae976e92e09a5385
DJDrama <putzzang@naver.com>
2022-03-12 17:23:04 +0900

    modification for fd

--- homework04/copy/cp_using_scattered:gathered/cp.c
+++ homework04/copy/cp_using_scattered:gathered/cp.c
@@ -14,24 +14,24 @@ int main(int argc, char *argv[]){
 	int iov_count, i;
 	struct iovec *iov;
 	ssize_t nr;
-	int fd;
+	int rfd, wfd;
 	int read_size = 8192;
 
 	t = clock();
 
 	/* open file for read only */
-	fd = open(argv[1], O_RDONLY);
-	if(fd == -1){
+	rfd = open(argv[1], O_RDONLY);
+	if(rfd == -1){
 		perror("open");
 		return 1;
 	}
 
 	/* get file size */
-	int file_size = lseek(fd, 0, SEEK_END);
+	int file_size = lseek(rfd, 0, SEEK_END);
 	iov_count = file_size / read_size;
 	if(file_size % read_size !=0)
 		iov_count++;
-	lseek(fd, 0, SEEK_SET);
+	lseek(rfd, 0, SEEK_SET);
 
 	/* malloc iov and buf */
 	iov = malloc(iov_count * sizeof(struct iovec));
@@ -47,28 +47,29 @@ int main(int argc, char *argv[]){
 	}
 
 	/* readv */
-	nr = readv(fd, iov, iov_count);
+	nr = readv(rfd, iov, iov_count);
 	if(nr == -1){
 		perror("readv");
 		return 1;
 	}
 
 	/* open file for write */
-	fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if(fd==-1){
+	wfd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if(wfd==-1){
 		perror("open");
 		return 1;
 	}
 
 	/* writev */
-	nr = writev(fd, iov, iov_count);
+	nr = writev(wfd, iov, iov_count);
 	if(nr==-1){
 		perror("writev");
 		return 1;
 	}
 
 
-	close(fd);
+	close(rfd);
+	close(wfd);
 
 	/* free memory */
 	for(i=0; i<iov_count; i++)
