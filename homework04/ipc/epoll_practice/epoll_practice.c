db3301e19f0bb8049b9933f52ebef4b26a19bad8
DJDrama <putzzang@naver.com>
2022-03-13 14:47:40 +0900

    epoll

--- homework04/ipc/epoll_practice/epoll_practice.c
+++ homework04/ipc/epoll_practice/epoll_practice.c
@@ -2,17 +2,24 @@
 #include <unistd.h>
 #include <sys/epoll.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MAX_EVENTS 64
 
 int main(){
-
+	time_t seconds;
+	struct tm *time_struct;
+	
 	struct epoll_event event, *events;
-	int nr_events, i, epfd;
+	int nr_events, i, epfd, read_bytes;
+	char buf[BUFSIZ];
 	int is_running=1;
 
+
+
 	/* epoll instance creation */
-	epfd = epoll_create1(); //  새로운 epoll 인스턴스 생성하고 연관된 파일 디스크립터를 반환.
+	epfd = epoll_create1(0); //  새로운 epoll 인스턴스 생성하고 연관된 파일 디스크립터를 반환.
 	if(epfd == -1){
 		perror("epoll_create1");
 		return 1;
@@ -20,7 +27,7 @@ int main(){
 
 	/* epoll control */
 	event.events = EPOLLIN; //  파일 읽기가 지연되지 않고 바로 가능한지 감시
-	event.data.fd = 0;
+	event.data.fd = 0; // stdin
 
 	if(epoll_ctl(epfd, EPOLL_CTL_ADD, 0, &event) < 0){
 		perror("epoll_ctl");
@@ -37,7 +44,11 @@ int main(){
 	}
 
 	while(is_running){
-		printf("Running...\n");
+		/* time init */
+		seconds = time(NULL);
+		time_struct = localtime(&seconds);
+		
+		printf("Waiting for input...\n");
 		nr_events = epoll_wait(epfd, events, MAX_EVENTS, -1); // timeout이 -1인 경우: 이벤트가 발생할 때까지 해당 호출은 반환되지 않음.
 		if(nr_events < 0){
 			perror("epoll_wait");
@@ -47,10 +58,25 @@ int main(){
 		}
 
 		for(i=0; i<nr_events; i++){
-			printf("event=%ld on fd=%d\n", events[i].events, events[i].data.fd);
+			read_bytes = read(events[i].data.fd, buf, BUFSIZ) ; // read stdin
+			if(read_bytes == -1){
+				perror("read");
+				free(events);
+				close(epfd);
+				return 1;
+			}
+			buf[read_bytes]='\0';
+			
+			printf("event=%d on fd=%d, read: %s(time: %d:%d:%d)\n\n", events[i].events, events[i].data.fd, buf, time_struct->tm_hour, time_struct->tm_min, time_struct->tm_sec);
+			
+			if(!strcasecmp(buf, "break\n")) { // stops when "break" is entered
+				is_running=0;
+			}
 		}
 	}
 
 	close(epfd); // epoll_create1()에서 반환하는 fd는 폴링이 끝난 뒤 반드시 close() 시스템 콜로 닫아줘야 한다.
 	free(events);
+	
+	return 0;
 }
