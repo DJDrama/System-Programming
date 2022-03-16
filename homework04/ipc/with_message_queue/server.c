106177bd86aa48f60065417a2ef5284326fef87c
DJDrama <putzzang@naver.com>
2022-03-16 14:57:44 +0900

    Updatees

--- homework04/ipc/with_message_queue/server.c
+++ homework04/ipc/with_message_queue/server.c
@@ -4,8 +4,8 @@
 #include <string.h>
 
 struct mymsgbuf {
-	long type;
-	char text[128];
+	long type; // 메세지 유형(양수)
+	char text[128]; // msgsnd() 함수의 msgsz로 지정한 크기의 버퍼로 메시지 내용이 저장됨.
 };
 
 int main(){
@@ -14,10 +14,14 @@ int main(){
 	struct mymsgbuf msg;
 	char input[128];
 
+
+	// returns a key base don path and id that is usable in subsequent calls to msget(), semget(), shmget()
+	// returns the same key value for all paths that name the same file, when called with the same id value.
 	key = ftok("keyfile", 1);
 
+
 	msgid = msgget(key, IPC_CREAT | 0644); // 메시지 큐 식별자 리턴(음수가 아닌 정수), IPC_CREATE: 새로운 키면 식별자를 새로 생성,
-	if(msgid==-1){
+	if(msgid==-1){ // 에러, 메시지 전송 하지 않음.
 		perror("msgget");
 		exit(1);
 	}
@@ -31,6 +35,7 @@ int main(){
 	msg.type=2;
 	strcpy(msg.text, input);
 
+	/* msgsend-> 식별자, 버퍼 주소, 크기, 0 = 블록 모드. */
 	if(msgsnd(msgid, (void *) &msg, sizeof(msg.text), 0) == -1){
 		perror("msgsnd");
 		exit(1);
