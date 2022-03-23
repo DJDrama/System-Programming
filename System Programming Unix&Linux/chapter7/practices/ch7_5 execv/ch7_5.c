dc23f67dc445d1a519174067856e529911f0374c
DJDrama <putzzang@naver.com>
2022-03-23 17:46:46 +0900

    exec results

--- System Programming Unix&Linux/chapter7/practices/ch7_5 execv/ch7_5.c
+++ System Programming Unix&Linux/chapter7/practices/ch7_5 execv/ch7_5.c
@@ -11,10 +11,16 @@ int main(){
 	argv[1] = "-a";
 	argv[2] = NULL;
 
-	if(execv("/usr/bin/ls", argv) == -1){
+	if(execv("/usr/bin/ls", argv) == -1){ // Only in linux (cannot find in Mac)
 		perror("execv");
 		exit(1);
 	}
 
 	printf("After exec function\n");
 }
+
+/*
+$ ./a.out 
+Before exec function
+.  ..  a.out  ch7_5.c
+*/
