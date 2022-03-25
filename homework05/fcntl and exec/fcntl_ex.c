d519f5407648c5c24524d1bffd29ff1e576069fd
DJDrama <putzzang@naver.com>
2022-03-26 03:09:49 +0900

    O_CLOEXEC

--- homework05/fcntl and exec/fcntl_ex.c
+++ homework05/fcntl and exec/fcntl_ex.c
@@ -1,26 +1,27 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h> // for perror
 
 int main(){
-	pid_t pid;
 	int fd;
 
-	fd = open("travel_wallet.txt", O_RDONLY);
-
-	switch(pid = fork()){
-		case -1: 
-			perror("fork");
-			exit(1);
-			break;
-		case 0:
-			printf("Child Process - My PID: %d, My Parent's PID: %d\n", (int) getpid(), (int) getppid());
-			printf("in Child fd: %d\n", fd);
-			break;
-		default : 
-			printf("Parent Process = My PID: %d, My Parent's PID: %d, My Child's PID: %d\n", (int) getpid(), (int) getppid(), (int) pid);
-			break;
+	fd = open("travel_wallet.txt", O_RDWR | O_CLOEXEC);
+	
+	if(fd==-1){
+		perror("open");
+		return -1;
 	}
-	printf("End of fork\n");
+	
+	if(execl("exec_file", "./exec_file", NULL) == -1){
+		perror("execl");
+		return -1;
+	}
+	
+	printf("End of fcntl_ex\n");
+	
+	return 0;
 }
+
+
