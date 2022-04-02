b7a39ff5894be915a7313c7fddd0561a6ad5887f
DJDrama <putzzang@naver.com>
2022-04-03 04:34:15 +0900

    chapter 06

---
+++ homework05/path injection/execlp/execlp.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	printf("<Before exec function>\n\n");
+
+	printf("# Command: ls -a\n");
+	if(execlp("ls", "ls", "-a", NULL) == -1){
+		perror("execlp");
+		exit(1);
+	}
+
+	printf("After exec function\n");  // 실행되지 않음: execlp 함수에서 프로세스 이미지가 ls 명령의 이미지로 바뀌었기 때문.
+}
